7bdc29fb9c3949f83e4c378ac79f5799de654a1b
tarutarusosu029 <test.reservation326@gmail.com>
2026-04-17 08:43:25 +0900

    test

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ async function getCalendarUrl() {
     let userAgent = window.navigator.userAgent.toLowerCase();
     if (userGrade && userClass && userGrade != 0 && userClass != 0) {
         try {
+            document.getElementById('userGrade').value = userGrade;
+            document.getElementById('userClass').value = userClass;
             const api = `https://script.google.com/macros/s/AKfycbzu83oiW4V8BkKVkwcgB4SyrE4EIf_7F6IwKD70tqO8CbkYehF3JyF1EcRkk83M-cOd/exec?action=getCalendar&userGrade=${userGrade}&userClass=${userClass}&userAgent=${userAgent}`;
             element = document.getElementById('status');
             if (element.classList.contains('hide')) {
@@ -50,6 +52,7 @@ function getParameter(paramName) {
 function updateStatus(html) {
     document.getElementById("status").innerHTML = html;
 }
+
 function updateGuide(html) {
     document.getElementById("guide").innerHTML = html;
 }
