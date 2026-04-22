ec72408a6a351039c5d084e9e036bfb1a6bb5b5f
tarutarusosu029 <test.reservation326@gmail.com>
2026-04-22 12:51:16 +0900

    fix

--- version.js
+++ version.js
@@ -1,3 +1,5 @@
-fetch('/version.txt?t=' + Date.now())
+fetch('/version.txt')
     .then(r => r.text())
-    .then(v => document.getElementById('v').textContent = v);
+    .then(v => {
+        document.getElementById('v').textContent = v.trim();
+    });
