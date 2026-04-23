38477d36c05b4b623295b631a4a72a60c8834fa0
tarutarusosu029 <test.reservation326@gmail.com>
2026-04-23 11:39:31 +0900

    wip

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ async function getCalendarUrl() {
             element = document.getElementById('status');
             if (element.classList.contains('hide')) {
                 toggleElementHide(element);
+                updateGuide(`<p>しばらくお待ちください</p>`);
             }
-            updateGuide(`<p>しばらくお待ちください</p>`);
             updateStatus(`<div class="loader"></div>`);
             const res = await fetch(apiReqUrl);
             const json = await res.json();
@@ -36,8 +36,9 @@ async function getCalendarUrl() {
             }
         } catch (e) {
             console.log(e);
+            updateGuide(`<p>時間をおいてからもう一度やり直してください</p>`);
             updateStatus(`<p>エラーが発生しました</p>`);
-            window.alert("エラー\n時間をおいてから、\nもう一度やり直してください。");
+            window.alert("エラー\n時間をおいてから\nもう一度やり直してください");
             action = "uploadErrorLog";
             apiReqUrl = `${api}?action=${action}&userGrade=${userGrade}&userClass=${userClass}&userAgent=${userAgent}&error=${e}`;
             await fetch(apiReqUrl);
